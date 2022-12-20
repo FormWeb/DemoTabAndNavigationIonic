6717c8f5abeacda364b52094a95ab136665e83b1
William <william.genard@bstorm.be>
2022-12-20 10:13:40 +0100

    fix

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -20,10 +20,13 @@ const routes: Routes = [
       {
         path: 'tab2',
         loadChildren: () => import('./components/tab2/tab2.module').then( m => m.Tab2PageModule)
+      },
+      {
+        path: 'tab3',
+        loadChildren: () => import('./components/tab3/tab3.module').then( m => m.Tab3PageModule)
       }
     ]
   }
-  
   
 ];
 
