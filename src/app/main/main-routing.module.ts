056b78a1fcbb7f327b74c73e4f32d5c52f0d53df
William <william.genard@bstorm.be>
2022-12-20 10:00:36 +0100

    first commit

---
+++ src/app/main/main-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MainPage } from './main.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'tab1',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    component: MainPage,
+    children: [
+      {
+        path: 'tab1',
+        loadChildren: () => import('./components/tab1/tab1.module').then( m => m.Tab1PageModule)
+      },
+      {
+        path: 'tab2',
+        loadChildren: () => import('./components/tab2/tab2.module').then( m => m.Tab2PageModule)
+      }
+    ]
+  }
+  
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class MainPageRoutingModule {}
